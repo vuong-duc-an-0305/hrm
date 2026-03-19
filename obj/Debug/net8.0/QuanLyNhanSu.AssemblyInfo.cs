e66855afd3dd1664703db59c64c59b8495ef1f13
An Vương Đức <vuongducan0305@gmail.com>
2026-03-19 16:09:14 +0700

    update .net

--- obj/Debug/net8.0/QuanLyNhanSu.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuanLyNhanSu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyNhanSu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2521af42cba2cdb504255921fa91fa613d13539e")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyNhanSu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyNhanSu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
